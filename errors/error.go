7cafd1e56b6c5c97ba6f207f53c942f81c0b8692
hdiomede <henrique.diomede@gmail.com>
2019-04-03 00:45:00 -0300

    flight model errors

--- errors/error.go
+++ errors/error.go
@@ -8,35 +8,57 @@ type AirportDoesNotExistsError struct {
 
 func (error *AirportDoesNotExistsError) Error() string {
 	return error.msg
- }
+}
 
- func AirportDoesNotExists(airportCode string) error {
+func AirportDoesNotExists(airportCode string) error {
 	return &AirportDoesNotExistsError{fmt.Sprintf("Airport %s does not exists", airportCode)}
- }
+}
 
- type FlightAlreadyExistsError struct {
-	 msg string
- }
+type FlightAlreadyExistsError struct {
+	msg string
+}
 
- func (error *FlightAlreadyExistsError) Error() string {
-	 return error.msg
- }
+func (error *FlightAlreadyExistsError) Error() string {
+	return error.msg
+}
 
- func FlightAlreadyExists() error {
-	 return &FlightAlreadyExistsError{"This flight already exists"}
- }
+func FlightAlreadyExists() error {
+	return &FlightAlreadyExistsError{"This flight already exists"}
+}
 
- type NoFlightFoundError struct {
-	 msg string
- }
+type NoFlightFoundError struct {
+	msg string
+}
 
- func (error *NoFlightFoundError) Error() string {
-	 return error.msg
- }
+func (error *NoFlightFoundError) Error() string {
+	return error.msg
+}
 
- func NoFlightFound() error {
+func NoFlightFound() error {
 	 return &NoFlightFoundError{"No flight found"}
- }
+}
 
+type InvalidAirportCodeFormatError struct {
+	msg string
+}
 
+func (error *InvalidAirportCodeFormatError) Error() string {
+	return error.msg
+}
+
+func InvalidAirportCodeFormat() {
+	return &InvalidAirportCodeFormatError{""}
+}
+
+type InvalidFlightCostError struct {
+	msg string
+}
+
+func (error *InvalidFlightCostError) Error() string {
+	return error.msg
+}
+
+func InvalidFlightCost() {
+	return &InvalidFlightCostError{""}
+}
 
